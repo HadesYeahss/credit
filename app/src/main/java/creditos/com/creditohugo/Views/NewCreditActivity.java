2d851a2292d6105702ca72f328c662e2bdd72dc3
Rigoberto Torres Honorato <rigoberto.torres@dextratech.com>
2018-01-30 15:15:36 -0600

    Alta UI terminado.

--- app/src/main/java/creditos/com/creditohugo/Views/NewCreditActivity.java
+++ app/src/main/java/creditos/com/creditohugo/Views/NewCreditActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 import java.sql.Date;
 import java.text.SimpleDateFormat;
@@ -22,40 +24,45 @@ import creditos.com.creditohugo.Objects.Cotizacion;
 import creditos.com.creditohugo.R;
 
 public class NewCreditActivity extends AppCompatActivity
-        implements View.OnClickListener, NewCreditControler.NewCreditIterface {
+        implements View.OnClickListener, NewCreditControler.NewCreditIterface{
 
     private NewCreditControler mController;
     private Calendar mCalendar;
     private DatePickerDialog.OnDateSetListener mDate;
-    private Toolbar mToolbar;
     public static final int RESULT_NEW_ID = 1;
 
     /*Campos de el formulario*/
     private EditText mName;
     private EditText mAmount;
     private EditText mInterest;
-    private EditText mNoPayments;
+    private Spinner mNoPayments;
     private RadioGroup mTypePayment;
     private RadioButton mMensual;
     private RadioButton mQuincenal;
+    private RadioButton mSemanal;
     private EditText mDatePayment;
     private Button mGuardar;
+    private int mNoPaymentsId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_credit);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_new_credit);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mController = new NewCreditControler(this);
-        mToolbar = (Toolbar) findViewById(R.id.toolbar_new_credit);
-        setSupportActionBar(mToolbar);
+
         mCalendar = Calendar.getInstance();
         mName = (EditText) findViewById(R.id.edit_text_credit_name);
         mAmount = (EditText) findViewById(R.id.edit_text_amount);
         mInterest = (EditText) findViewById(R.id.edit_text_annual_interest);
-        mNoPayments = (EditText) findViewById(R.id.edit_text_number_of_payments);
+        mNoPayments = (Spinner) findViewById(R.id.spinner_number_of_payments);
         mTypePayment = (RadioGroup) findViewById(R.id.tipo_pago);
         mMensual = (RadioButton) findViewById(R.id.radio_button_mensual);
         mQuincenal = (RadioButton) findViewById(R.id.radio_button_quincenal);
+        mSemanal = (RadioButton) findViewById(R.id.radio_button_semanal);
         mDatePayment = (EditText) findViewById(R.id.edit_text_date_of_payments);
         mGuardar = (Button) findViewById(R.id.btn_guardar);
         mGuardar.setOnClickListener(this);
@@ -80,6 +87,10 @@ public class NewCreditActivity extends AppCompatActivity
                         mCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.number_of_payments_array, R.layout.spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mNoPayments.setAdapter(adapter);
     }
 
     private void updateLabel() {
@@ -98,15 +109,15 @@ public class NewCreditActivity extends AppCompatActivity
                     cotizacion.setName(mName.getText().toString());
                     cotizacion.setAmount(Double.parseDouble(mAmount.getText().toString()));
                     cotizacion.setInterest(Double.parseDouble(mInterest.getText().toString()));
-                    cotizacion.setNoPayments(Integer.parseInt(mNoPayments.getText().toString()));
-                    cotizacion.setTypePayment(true);
+                    cotizacion.setNoPayments(Integer.parseInt(mNoPayments.getSelectedItem().toString()));
                     long dateMil = mCalendar.getTimeInMillis();
                     if (mMensual.isChecked()) {
-                        cotizacion.setTypePayment(true);
-                    } else {
-                        cotizacion.setTypePayment(false);
+                        cotizacion.setTypePayment(1);
+                    } else if (mQuincenal.isChecked()) {
+                        cotizacion.setTypePayment(2);
+                    } else{
+                        cotizacion.setTypePayment(3);
                     }
-                    cotizacion.setTypePayment(false);
                     Date date = new Date(dateMil);
                     cotizacion.setDatePayment(date);
                     mController.createCotizacion(cotizacion);
@@ -126,6 +137,5 @@ public class NewCreditActivity extends AppCompatActivity
         setResult(RESULT_OK, data);
         finish();
     }
-
 
 }
